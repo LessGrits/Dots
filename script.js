fb6add9c23ddf886cfa04668c7594bf972ab248a
Oles <ruboken@gmail.com>
2019-10-02 22:46:25 +0300

    beta

--- script.js
+++ script.js
@@ -1,32 +1,80 @@
 let field = document.querySelector('#field');
-
-window.onload = ()=>{
-    createField();
-    paintSquare();
+let playBtn = document.querySelector('#start');
+let restartBtn = document.querySelector('#restart');
+let countField =  document.querySelector('#count');
+let count = 0;
+let botCount = 0;
+playBtn.onclick = gamingProces;
+restartBtn.onclick = () => {
 
 };
 
-function createField(){
-    let squareList = [];
 
-    for(let i = 0 ; i<5 ; i++){
-        squareList[i] = [];
-        for(let j = 0 ; j<5 ; j++){
-            squareList[i][j]= j;
-            field.innerHTML += "<div class='square'></div>";
+function gamingProces() {
+    getStartData();
+    createField();
+    autoPaint();
+    paintSquare();
+}
+
+
+function getStartData() {
+
+    playBtn.disabled = true;
+}
+
+
+function createField() {
+    let squares = '';
+    for (let i = 0, squareList = []; i < 25; i++) {
+        squareList[i] = i;
+        squares += "<div class='square'></div>";
+    }
+    field.innerHTML = squares
+}
+
+function paintSquare() {
+    field.onclick = (event) => {
+        let div = event.target;
+        if (div.classList.contains('square')) {
+            if (div.style.backgroundColor == 'blue') {
+                div.style.backgroundColor = "green";
+                count++;
+                countField.innerHTML = `${count}/${botCount}`
+            }
         }
     }
 }
 
-function paintSquare() {
-    field.onclick = (event)=>{
-        console.log(event);
-        let div = event.target;
-        div.style.backgroundColor = "red";
+function random() {
+    return Math.floor(Math.random() * 25);
+}
+
+function autoPaint() {
+    let squares = document.getElementsByClassName('square');
+    setTimeout(paint, 500);
+    let rand = random();
+    function paint() {
+
+        if (squares[rand].style.backgroundColor !== 'green' && squares[rand].style.backgroundColor !== 'blue' && squares[rand].style.backgroundColor !== 'red') {
+            squares[rand].style.backgroundColor = 'blue';
+            autoPaint();
+
+        } else {
+            paint();
+        }
     }
 
+    setTimeout(() => redPaint(rand), 1100);
+
+    function redPaint(r) {
+        if (squares[r].style.backgroundColor !== 'green') {
+            squares[r].style.backgroundColor = 'red';
+            botCount++;
+            countField.innerHTML = `${count}/${botCount}`
+        }
+    }
 
 }
-console.log(Math.floor(Math.random()*25));
 
-
+// TODO use addEventListener!
